a93eff610b43c6dda9f9528eeab44661c28a8b20
zeckem <limzeming11@gmail.com>
2020-06-26 16:13:05 +0800

    tutorial from travesty

---
+++ src/components/PostForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPosts } from '../actions/postActions';
+
+class PostForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title:'',
+            body:''
+        };
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onChange(e){
+        this.setState({[e.target.name]:e.target.value});
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+        this.props.createPosts(post);
+    }
+    render() {
+        return (
+            <div>
+                <h1> Add Posts </h1>
+                <form onSubmit={this.onSubmit}>
+                    <div>
+                        <label>Title: </label><br />
+                        <input type="text" name="title" onChange={this.onChange} value={this.state.title}/>
+                    </div> 
+                    <br />
+                    <div>
+                        <label>Body: </label><br />
+                        <textarea name="body" onChange={this.onChange} value={this.state.body}/>  
+                    </div>
+                    <br />  
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+PostForm.propTypes = {
+    createPosts: PropTypes.func.isRequired
+}
+export default connect(null, {createPosts})(PostForm);
